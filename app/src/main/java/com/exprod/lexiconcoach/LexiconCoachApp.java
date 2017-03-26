95988c6fdf41574bc0f7a7a17d36a6f6c39c817e
VladMoroz <example.production@gmail.com>
2017-03-26 16:28:28 +0300

    Добавлен синглтон LexiconCoachApp + удален стандартный PlaceholderFragment

---
+++ app/src/main/java/com/exprod/lexiconcoach/LexiconCoachApp.java
@@ -0,0 +1,14 @@
+package com.exprod.lexiconcoach;
+
+import android.app.Application;
+
+/**
+ * Created by VLAD on 26.03.2017.
+ */
+
+public class LexiconCoachApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
